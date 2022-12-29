20f6a562608d8f164489534edbfed7c10e78c007
MUHAMMAD RIFKY <muhammad26rifky06@gmail.com>
2022-12-29 11:17:59 +0700

    PHP

---
+++ function.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fungsi</title>
+</head>
+<body>
+    <h2>penggunaan Fungsi</h2>
+    <?php
+    function luas($a,$b)
+    {
+        $hasil=$a*$b;
+        return($hasil);
+    }
+    ?>
+</body>
+</html>
